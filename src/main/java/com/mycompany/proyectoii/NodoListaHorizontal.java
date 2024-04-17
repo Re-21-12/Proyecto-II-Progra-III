9b9fce70122990c3592825e47505a12fac8d0801
Victor Macario <104967229+Re-21-12@users.noreply.github.com>
2024-04-17 12:45:41 -0600

    Add files via upload

--- src/main/java/com/mycompany/proyectoii/NodoListaHorizontal.java
+++ src/main/java/com/mycompany/proyectoii/NodoListaHorizontal.java
@@ -4,107 +4,99 @@
  */
 package com.mycompany.proyectoii;
 
-/**
- *
- * @author victo
- */
+
 //se mueven en horizontal
 public class NodoListaHorizontal {
 
-    private NodoOrtogonal cabeza;
-    private NodoOrtogonal cola;
+    private NodoOrtogonal cabeza; // Declara una variable privada cabeza del tipo NodoOrtogonal
+    private NodoOrtogonal cola; // Declara una variable privada cola del tipo NodoOrtogonal
 
     //utilizamos listas doblemente enlazadas por lo cual deben apuntar a nulo
-    public NodoListaHorizontal() {
-        this.cabeza = this.cola = null;
+    public NodoListaHorizontal() { // Constructor de la clase NodoListaHorizontal
+        this.cabeza = this.cola = null; // Inicializa cabeza y cola como nulos
     }
 
-    public void insertarEnHorizontal(NodoOrtogonal nodoHorizontal) {
+    public void insertarEnHorizontal(NodoOrtogonal nodoHorizontal) { // Método para insertar un nodo en la lista horizontal
         //si la cabeza esta vacia entonces podemos asignar un nuevo elemento
-        if (cabeza == null) {
-            this.cabeza = this.cola = nodoHorizontal;
+        if (cabeza == null) { // Verifica si la cabeza está vacía
+            this.cabeza = this.cola = nodoHorizontal; // Asigna el nuevo nodo como cabeza y cola
             //cocmo vamos aumentando segun se van ingresando al principio estaran los menores y de ultimo los mayores
-        } else {
-            if (nodoHorizontal.getX() < cabeza.getX()) {
-//si es mayor que el final va de ultimo osea es nuevo
-                cabeza = insertarInicio(nodoHorizontal);
-            } else if (nodoHorizontal.getX() > cola.getX()) {
-                //si no es ni mayor ni menor entonces va en medioo
-                cola = insertarFinal(nodoHorizontal);
-
-            } else {
-                insertarEnMedio(nodoHorizontal);
+        } else { // Si la cabeza no está vacía
+            if (nodoHorizontal.getX() < cabeza.getX()) { // Verifica si el nuevo nodo tiene un valor menor que el de la cabeza
+                cabeza = insertarInicio(nodoHorizontal); // Inserta el nuevo nodo al inicio de la lista
+            } else if (nodoHorizontal.getX() > cola.getX()) { // Verifica si el nuevo nodo tiene un valor mayor que el de la cola
+                cola = insertarFinal(nodoHorizontal); // Inserta el nuevo nodo al final de la lista
+            } else { // Si el nuevo nodo tiene un valor entre la cabeza y la cola
+                insertarEnMedio(nodoHorizontal); // Inserta el nuevo nodo en medio de la lista
             }
 
         }
 
     }
 
-//aqui los valores se van empujando de izquierda a derecha hasta llegar a null
+    //aqui los valores se van empujando de izquierda a derecha hasta llegar a null
     // 1 <- 2 <- null
-    private NodoOrtogonal insertarInicio(NodoOrtogonal nodoHorizontal) {
+    private NodoOrtogonal insertarInicio(NodoOrtogonal nodoHorizontal) { // Método para insertar un nodo al inicio de la lista
         // establecemos los enlaces del nuevo nodo hacia la cabeza y viceversa
-        nodoHorizontal.setDerecha(cabeza);
-        if (cabeza != null) {
-            cabeza.setIzquierda(nodoHorizontal);
+        nodoHorizontal.setDerecha(cabeza); // Establece el enlace hacia la cabeza
+        if (cabeza != null) { // Verifica si la cabeza no es nula
+            cabeza.setIzquierda(nodoHorizontal); // Establece el enlace hacia el nuevo nodo
         }
         // colocamos el nuevo nodo como cabeza de la lista
-        cabeza = nodoHorizontal;
-        return cabeza;
+        cabeza = nodoHorizontal; // Asigna el nuevo nodo como cabeza
+        return cabeza; // Devuelve la cabeza actualizada
     }
 
-//aqui los valores se van empujando de derecha a izquierda inicando con null
+    //aqui los valores se van empujando de derecha a izquierda inicando con null
     // nuull -> 2 -> 1 
-    private NodoOrtogonal insertarFinal(NodoOrtogonal nodoHorizontal) {
+    private NodoOrtogonal insertarFinal(NodoOrtogonal nodoHorizontal) { // Método para insertar un nodo al final de la lista
         // si la cabeza está vacía, asignamos el nuevo nodo como cola
-        if (cabeza == null) {
-            return nodoHorizontal;
+        if (cabeza == null) { // Verifica si la cabeza es nula
+            return nodoHorizontal; // Devuelve el nuevo nodo como cola
         }
         // si no está vacía, recorremos la lista hasta llegar al último nodo
-        NodoOrtogonal actual = cabeza;
-        while (actual.getDerecha() != null) {
-            actual = actual.getDerecha();
+        NodoOrtogonal actual = cabeza; // Inicializa actual como la cabeza
+        while (actual.getDerecha() != null) { // Itera hasta llegar al último nodo
+            actual = actual.getDerecha(); // Avanza al siguiente nodo
         }
         // establecemos los enlaces del último nodo hacia el nuevo nodo y viceversa
-        actual.setDerecha(nodoHorizontal);
-        nodoHorizontal.setIzquierda(actual);
+        actual.setDerecha(nodoHorizontal); // Establece el enlace hacia el nuevo nodo
+        nodoHorizontal.setIzquierda(actual); // Establece el enlace hacia el nodo anterior
         // colocamos el nuevo nodo como cola de la lista
-        cola = nodoHorizontal;
-        return cola;
+        cola = nodoHorizontal; // Asigna el nuevo nodo como cola
+        return cola; // Devuelve la cola actualizada
     }
 
-    private NodoOrtogonal insertarEnMedio(NodoOrtogonal nodoHorizontal) {
+    private NodoOrtogonal insertarEnMedio(NodoOrtogonal nodoHorizontal) { // Método para insertar un nodo en medio de la lista
         // buscamos el nodo después del cual se insertará el nuevo nodo
-        NodoOrtogonal actual = cabeza;
-        while (actual != null && nodoHorizontal.getX() > actual.getX()) {
-            actual = actual.getDerecha();
+        NodoOrtogonal actual = cabeza; // Inicializa actual como la cabeza
+        while (actual != null && nodoHorizontal.getX() > actual.getX()) { // Itera hasta encontrar el nodo adecuado
+            actual = actual.getDerecha(); // Avanza al siguiente nodo
         }
         // si no se encuentra el nodo después del cual se insertará el nuevo nodo, no hacemos nada
-        if (actual == null) {
-            System.out.println("El valor después del cual se desea insertar no se encontró en la lista.");
-            return cabeza;
+        if (actual == null) { // Verifica si no se encontró el nodo
+            System.out.println("El valor después del cual se desea insertar no se encontró en la lista."); // Imprime un mensaje de error
+            return cabeza; // Devuelve la cabeza sin cambios
         }
         // insertamos el nuevo nodo después del nodo actual
-        NodoOrtogonal anterior = actual.getIzquierda();
-        nodoHorizontal.setDerecha(actual);
-        nodoHorizontal.setIzquierda(anterior);
-        actual.setIzquierda(nodoHorizontal);
-        if (anterior != null) {
-            anterior.setDerecha(nodoHorizontal);
-        } else {
-            cabeza = nodoHorizontal;
+        NodoOrtogonal anterior = actual.getIzquierda(); // Obtiene el nodo anterior al nodo actual
+        nodoHorizontal.setDerecha(actual); // Establece el enlace hacia el nodo actual
+        nodoHorizontal.setIzquierda(anterior); // Establece el enlace hacia el nodo anterior
+        actual.setIzquierda(nodoHorizontal); // Establece el enlace desde el nodo actual hacia el nuevo nodo
+        if (anterior != null) { // Verifica si el nodo anterior no es nulo
+            anterior.setDerecha(nodoHorizontal); // Establece el enlace desde el nodo anterior hacia el nuevo nodo
+        } else { // Si el nodo anterior es nulo, significa que el nuevo nodo es la nueva cabeza
+            cabeza = nodoHorizontal; // Asigna el nuevo nodo como cabeza
         }
-        return cabeza;
+        return cabeza; // Devuelve la cabeza actualizada
     }
 
-    public void mostrarLista() {
-        NodoOrtogonal actual = cabeza;
-        while (actual != null) {
+    public void mostrarLista() { // Método para mostrar la lista horizontal
+        NodoOrtogonal actual = cabeza; // Inicializa actual como la cabeza
+        while (actual != null) { // Itera hasta llegar al final de la lista
             System.out.print("|" + actual.getX() + "|"); // Imprime el valor del nodo actual
             actual = actual.getDerecha(); // Avanza al siguiente nodo
         }
-        System.out.println(); // agregamos "null" al final de la lista
-
-//System.out.println("null"); // Agrega "null" al final de la lista
+        System.out.println(); // Imprime un salto de línea al final de la lista
     }
 }
--- src/main/java/com/mycompany/proyectoii/NodoX.java
+++ src/main/java/com/mycompany/proyectoii/NodoX.java
@@ -9,64 +9,59 @@ package com.mycompany.proyectoii;
  * @author victo
  */
 public class NodoX {
-    private Vehiculo carro;
-    private int y;//su coordenada en x
-    private NodoX arriba;
-    private NodoX abajo;
-    //cada nodoo Columna contiene una lista de fila 
-    private NodoListaHorizontal filas; 
 
-    public NodoX(int y, Vehiculo carro) {
-        this.y = y;
-        this.carro = carro;
-        //creamos una columna por cada nodofila
-        this.filas = new NodoListaHorizontal();
-        this.arriba = this.abajo = null;
+    private Vehiculo carro; // Declara una variable privada carro de tipo Vehiculo
+    private int y; // Declara una variable privada y de tipo entero, representa la coordenada en y
+    private NodoX arriba; // Declara una variable privada arriba de tipo NodoX, representa el nodo ubicado arriba del nodo actual
+    private NodoX abajo; // Declara una variable privada abajo de tipo NodoX, representa el nodo ubicado abajo del nodo actual
+    //cada nodo Columna contiene una lista de fila
+    private NodoListaHorizontal filas; // Declara una variable privada filas de tipo NodoListaHorizontal, representa la lista horizontal asociada al nodo actual
+
+    public NodoX(int y, Vehiculo carro) { // Constructor de la clase NodoX con parámetros
+        this.y = y; // Inicializa la variable y con el parámetro y
+        this.carro = carro; // Inicializa la variable carro con el parámetro carro
+        //creamos una columna por cada nodo fila
+        this.filas = new NodoListaHorizontal(); // Inicializa la variable filas como una nueva instancia de NodoListaHorizontal
+        this.arriba = this.abajo = null; // Inicializa las variables arriba y abajo como nulas
     }
 
-    public Vehiculo getCarro() {
-        return carro;
+    public Vehiculo getCarro() { // Método para obtener el vehículo asociado al nodo
+        return carro; // Retorna el vehículo asociado al nodo
     }
 
-    public void setCarro(Vehiculo carro) {
-        this.carro = carro;
+    public void setCarro(Vehiculo carro) { // Método para establecer el vehículo asociado al nodo
+        this.carro = carro; // Asigna el vehículo con el parámetro carro
     }
 
-    public int getY() {
-        return y;
+    public int getY() { // Método para obtener el valor de y (coordenada en y)
+        return y; // Retorna el valor de y
     }
 
-    public void setY(int y) {
-        this.y = y;
+    public void setY(int y) { // Método para establecer el valor de y (coordenada en y)
+        this.y = y; // Asigna el valor de y con el parámetro y
     }
 
-    public NodoX getArriba() {
-        return arriba;
+    public NodoX getArriba() { // Método para obtener el nodo ubicado arriba del nodo actual
+        return arriba; // Retorna el nodo ubicado arriba del nodo actual
     }
 
-    public void setArriba(NodoX arriba) {
-        this.arriba = arriba;
+    public void setArriba(NodoX arriba) { // Método para establecer el nodo ubicado arriba del nodo actual
+        this.arriba = arriba; // Asigna el nodo ubicado arriba del nodo actual
     }
 
-    public NodoX getAbajo() {
-        return abajo;
+    public NodoX getAbajo() { // Método para obtener el nodo ubicado abajo del nodo actual
+        return abajo; // Retorna el nodo ubicado abajo del nodo actual
     }
 
-    public void setAbajo(NodoX abajo) {
-        this.abajo = abajo;
+    public void setAbajo(NodoX abajo) { // Método para establecer el nodo ubicado abajo del nodo actual
+        this.abajo = abajo; // Asigna el nodo ubicado abajo del nodo actual
     }
 
-    public NodoListaHorizontal getFilas() {
-        return filas;
+    public NodoListaHorizontal getFilas() { // Método para obtener la lista horizontal asociada al nodo actual
+        return filas; // Retorna la lista horizontal asociada al nodo actual
     }
 
-    public void setFilas(NodoListaHorizontal filas) {
-        this.filas = filas;
+    public void setFilas(NodoListaHorizontal filas) { // Método para establecer la lista horizontal asociada al nodo actual
+        this.filas = filas; // Asigna la lista horizontal con el parámetro filas
     }
-
-
-
- 
-
-    
 }
